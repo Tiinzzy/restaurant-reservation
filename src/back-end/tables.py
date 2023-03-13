a509f3bfd364ac2a7213c74bc41775d57e4a1115
Tina <tinzzy.cracked@yahoo.com>
2023-03-13 11:17:36 -0400

    added method to create table for reservation

--- src/back-end/tables.py
+++ src/back-end/tables.py
@@ -104,3 +104,30 @@ class Tables:
         cur.execute(create_table_sql)
         con.commit()
         con.close()
+
+    @classmethod
+    def create_reservation(self):
+        db = Database()
+        con, cur = db.open_database()
+        cur.execute("drop table if exists tests.rr_reservation")
+        create_table_sql = """
+        create tests.rr_reservation (
+                id int NOT NULL AUTO_INCREMENT,
+                timestamp varchar(100),
+                customer_name varchar(20),
+                customer_id int,
+                name varchar(20),
+                seat_count int,
+                table_id int,
+                for_date varchar(100),
+                for_how_long varchar(100),
+                status varchar(100),
+                latest_comment varchar(100),
+                waiter_id int,
+                total_price int,
+                tip_percent int
+                PRIMARY KEY (id)
+                )"""
+        cur.execute(create_table_sql)
+        con.commit()
+        con.close()    
