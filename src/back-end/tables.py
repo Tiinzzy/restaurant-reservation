630ff8e34ba2b4a98fd650397c52c9023f45e288
Tina <tinzzy.cracked@yahoo.com>
2023-03-13 10:15:47 -0400

    added method to create relational table for users and their id

--- src/back-end/tables.py
+++ src/back-end/tables.py
@@ -12,9 +12,23 @@ class Tables:
         cur.execute("drop table if exists tests.rr_roles")
         create_table_sql = """
         create table tests.rr_roles (
-                id int,
+                id int NOT NULL AUTO_INCREMENT,
                 name(150)
                 )"""
         cur.execute(create_table_sql)
         con.commit()
         con.close()
+
+    @classmethod
+    def create_user_roles(self):
+        db = Database()
+        con, cur = db.open_database()
+        cur.execute("drop table if exists tests.rr_user_roles")
+        create_table_sql = """
+        create tests.rr_user_roles (
+                user_id int,
+                role_id int
+                )"""
+        cur.execute(create_table_sql)
+        con.commit()
+        con.close()    
