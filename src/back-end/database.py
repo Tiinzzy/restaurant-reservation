fc6bf96a2a18356d240d39df5ac3e5cb210139fb
Tina <tinzzy.cracked@yahoo.com>
2023-03-13 10:09:06 -0400

    created module to connect to database

---
+++ src/back-end/database.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+class Database:
+    @classmethod
+    def open_database(self):
+        Database.con = mysql.connector.connect(
+            user='dbadmin', password='washywashy', host='127.0.0.1', database='tests')
+        return Database.con, Database.con.cursor()
+
+    @classmethod
+    def close_database(self):
+        Database.con.cursor().close()
+        Database.con.close()
+
