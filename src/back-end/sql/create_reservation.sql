6bb08346f6e44a2680cef80e060d949269546697
Tina <tinzzy.cracked@yahoo.com>
2023-03-14 09:57:53 -0400

    sql commands to create and drop reservation table in MySQL

---
+++ src/back-end/sql/create_reservation.sql
@@ -0,0 +1,16 @@
+create table tests.rr_reservation (
+    id int NOT NULL AUTO_INCREMENT,
+    timestamp varchar(100),
+    customer_name varchar(20),
+    customer_id int,
+    name varchar(20),
+    seat_count int,
+    table_id int,
+    for_date varchar(100),
+    for_how_long varchar(100),
+    status varchar(100),
+    latest_comment varchar(100),
+    waiter_id int,
+    total_price int,
+    tip_percent int PRIMARY KEY (id)
+);
