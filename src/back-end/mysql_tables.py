88f993441b934b8310e6c0680d545c5b9c40460b
Tina <tinzzy.cracked@yahoo.com>
2023-03-14 09:58:32 -0400

    fixed execution

--- src/back-end/mysql_tables.py
+++ src/back-end/mysql_tables.py
@@ -81,25 +81,7 @@ class MysqlTables:
     def create_reservation(self):
         db = Database()
         con, cur = db.open_database()
-        cur.execute("drop table if exists tests.rr_reservation")
-        create_table_sql = """
-        create table tests.rr_reservation (
-                id int NOT NULL AUTO_INCREMENT,
-                timestamp varchar(100),
-                customer_name varchar(20),
-                customer_id int,
-                name varchar(20),
-                seat_count int,
-                table_id int,
-                for_date varchar(100),
-                for_how_long varchar(100),
-                status varchar(100),
-                latest_comment varchar(100),
-                waiter_id int,
-                total_price int,
-                tip_percent int
-                PRIMARY KEY (id)
-                )"""
-        cur.execute(create_table_sql)
+        cur.execute(read_sql_file('drop_reservation.sql'))
+        cur.execute(read_sql_file('create_reservation.sql'))
         con.commit()
         con.close()
