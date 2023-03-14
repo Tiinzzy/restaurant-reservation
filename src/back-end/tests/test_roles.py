6fd0bf1a86526f957a3fc6a3958f3616036aed98
Tina <tinzzy.cracked@yahoo.com>
2023-03-14 13:45:03 -0400

    fixed testing

--- src/back-end/tests/test_roles.py
+++ src/back-end/tests/test_roles.py
@@ -7,24 +7,31 @@ class TestRoles(unittest.TestCase):
 
     def test_drop_create_table(self):
         MysqlTables.create_roles()
-        row_count1 = MysqlTables.select_roles_count()
+        row_count = MysqlTables.select_roles_count()
+        print(row_count)
 
         MysqlTables.create_user_roles()
-        row_count2 = MysqlTables.select_user_roles_count()
-        
+        row_count = MysqlTables.select_user_roles_count()
+        print(row_count)
+
         MysqlTables.create_user()
-        row_count3 = MysqlTables.select_user_count()
+        row_count = MysqlTables.select_user_count()
+        print(row_count)
 
         MysqlTables.create_menu_items()
-        row_count4 = MysqlTables.select_menu_items_count()
+        row_count = MysqlTables.select_menu_items_count()
+        print(row_count)
 
         MysqlTables.create_order_items()
-        row_count5 = MysqlTables.select_order_items_count()
+        row_count = MysqlTables.select_order_items_count()
+        print(row_count)
 
         MysqlTables.create_seating_tables()
-        row_count6 = MysqlTables.select_seating_tables_count()
+        row_count = MysqlTables.select_seating_tables_count()
+        print(row_count)
 
         MysqlTables.create_reservation()
-        row_count7 = MysqlTables.select_reservation_count()
-  
-        self.assertEqual(row_count7, 0, "test_drop_create_table Failed!")
+        row_count = MysqlTables.select_reservation_count()
+        print(row_count)
+
+        self.assertEqual(row_count, 0, "test_drop_create_table Failed!")
