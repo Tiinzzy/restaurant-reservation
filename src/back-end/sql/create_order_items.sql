f999683a92a11af34d00b44ad23c57b400f45e61
Tina <tinzzy.cracked@yahoo.com>
2023-03-14 09:51:57 -0400

    create and drop oder items table MySQL command

---
+++ src/back-end/sql/create_order_items.sql
@@ -0,0 +1,8 @@
+create table tests.rr_order_items (
+    id int NOT NULL AUTO_INCREMENT,
+    reservation_id int,
+    menu_item_id int,
+    count int,
+    total_price int,
+    comment varchar(200)
+);
