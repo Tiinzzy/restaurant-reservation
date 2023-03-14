5abb7c9c0463ac8491635ce83fb98ae782ec501f
Tina <tinzzy.cracked@yahoo.com>
2023-03-14 09:40:08 -0400

    function to read mysql command from sql folder

--- src/back-end/tests/test_roles.py
+++ src/back-end/tests/test_roles.py
@@ -1,11 +1,16 @@
 import unittest
 
-from mysql_tables import Tables
+from mysql_tables import MysqlTables
 
 
 class TestRoles(unittest.TestCase):
     
     def test_drop_create_table(self):
-        Tables.create_roles()
-        row_count = Tables.select_roles_count()
+        MysqlTables.create_roles()
+        row_count = MysqlTables.select_roles_count()
+
+        # MysqlTables.create_user_roles()
+        # row_count = MysqlTables.select_user_roles_count()
         self.assertEqual(row_count, 0, "test_drop_create_table Failed!")
+
+
