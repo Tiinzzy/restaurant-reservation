a68e97fecb5481f72cfe1617cb00b7a004e4d7d6
Tina <tinzzy.cracked@yahoo.com>
2023-03-15 14:16:05 -0400

    added delete sql command

--- src/back-end/seatin_class_sql.py
+++ src/back-end/seatin_class_sql.py
@@ -1,4 +1,8 @@
 add_sql = """
 INSERT INTO tests.rr_seating_tables (seat_count, available)
-    VALUES (%s,%s)
+    VALUES (%s,%s);
+"""
+
+delete_sql = """
+DELETE FROM tests.rr_seating_tables WHERE id = %s ;
 """
