2a3b7409171d823959f03051d010f41c7725a865
Tina <tinzzy.cracked@yahoo.com>
2023-03-15 15:25:26 -0400

    added sql command for deleting an order item

--- src/back-end/order_items_class_sql.py
+++ src/back-end/order_items_class_sql.py
@@ -2,3 +2,7 @@ add_sql = """
 INSERT INTO tests.rr_order_items (reservation_id, menu_item_id, count, total_price, comment)
     VALUES(%s,%s,%s,%s,%s);
 """
+
+delete_sql = """
+DELETE FROM tests.rr_order_items WHERE menu_item_id = %s
+"""
