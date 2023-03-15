363ed8447260d4a2971a4718a50a7bf7854c9939
Tina <tinzzy.cracked@yahoo.com>
2023-03-15 14:28:05 -0400

    ADDED SQL COMMAND TO SELECT TABLE

--- src/back-end/seatin_class_sql.py
+++ src/back-end/seatin_class_sql.py
@@ -6,3 +6,9 @@ INSERT INTO tests.rr_seating_tables (seat_count, available)
 delete_sql = """
 DELETE FROM tests.rr_seating_tables WHERE id = %s ;
 """
+
+select_sql = """
+SELECT id FROM tests.rr_seating_tables
+    WHERE available = 'True' AND seat_count = %s 
+    LIMIT 1;
+"""
