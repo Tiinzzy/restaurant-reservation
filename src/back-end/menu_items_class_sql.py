79c523f22bd1eaf08d6ab8e310c0c6c1ca9a9e4a
Tina <tinzzy.cracked@yahoo.com>
2023-03-15 14:52:54 -0400

    add and delete sql command

--- src/back-end/menu_items_class_sql.py
+++ src/back-end/menu_items_class_sql.py
@@ -2,3 +2,7 @@ add_sql = """
 INSERT INTO tests.rr_menu_items (name, category, price, description)
     VALUES(%s,%s,%s,%s);
 """
+
+delete_sql = """
+DELETE FROM tests.rr_menu_items WHERE id = %s
+"""
