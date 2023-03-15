a77edd03ea9d59dd3b385e1ec0d5356465d62a3e
Tina <tinzzy.cracked@yahoo.com>
2023-03-15 13:23:46 -0400

    made some changes to table creation of reservation

--- src/back-end/create_tables_sql.py
+++ src/back-end/create_tables_sql.py
@@ -27,7 +27,6 @@ create table tests.rr_reservation (
     timestamp varchar(100),
     customer_name varchar(20),
     customer_id int,
-    name varchar(20),
     seat_count int,
     table_id int,
     for_date varchar(100),
@@ -35,6 +34,7 @@ create table tests.rr_reservation (
     status varchar(100),
     latest_comment varchar(100),
     waiter_id int,
+    reservation_type varchar(100),
     total_price int,
     tip_percent int,
     PRIMARY KEY (id)
