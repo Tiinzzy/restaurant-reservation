8468417216a93f9671b7ecabb5476ee0d327a59d
Tina <tinzzy.cracked@yahoo.com>
2023-03-15 14:52:29 -0400

    fixed add and delete method to execute sql and send correct data

--- src/back-end/menu_items.py
+++ src/back-end/menu_items.py
@@ -1,37 +1,36 @@
 from database import Database
 
+import menu_items_class_sql as menu_items_table
+
 
 class MenuItems:
     def __init__(self):
         pass
 
     @classmethod
-    def add(self, name, category, price, description):
+    def add(self, info):
+        data = (info['name'],
+                info['category'],
+                info['price'],
+                info['description'])
         db = Database()
         con, cur = db.open_database()
-        sql = """INSERT INTO tests.rr_menu_items (name, category, price, description)
-                VALUES(%s,%s,%s,%s)"""
-        data = (name, category, price, description)
-        cur.execute(sql, data)
-        rows = cur.fetchall()
+        cur.execute(menu_items_table.add_sql, data)
         con.commit()
-        result = []
-        for row in rows:
-            result.append({'result': row[0]})
+        result = {'count': cur.rowcount}
         db.close_database()
-
         return result
 
     @classmethod
-    def delete(self, name):
+    def delete(self, id):
         db = Database()
         con, cur = db.open_database()
-        sql = """DELETE FROM tests.rr_menu_items WHERE name = '%s'"""
-        cur.execute(sql, name)
+        cur.execute(menu_items_table.delete_sql, (int(id),))
+        con.commit()
         rows = cur.fetchall()
-        result = []
-        for row in rows:
-            result.append({'result': row[0]})
+        result = {}
+        if len(rows) == 1:
+            result['data_row'] = rows[0]
         db.close_database()
         return result
 
@@ -42,9 +41,9 @@ class MenuItems:
         sql = """SELECT * FROM tests.rr_menu_items WHERE"""
         cur.execute(sql)
         rows = cur.fetchall()
-        result = []
-        for row in rows:
-            result.append({'all_menu_items': row[0]})
+        result = {}
+        if len(rows) == 1:
+            result['data_row'] = rows[0]
         db.close_database()
         return result
 
