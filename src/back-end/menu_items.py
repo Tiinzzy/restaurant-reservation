4d8f0a16b0104d8e70c07795ded6d3018e2b721d
Tina <tinzzy.cracked@yahoo.com>
2023-03-16 11:18:55 -0400

    created class object and returning results as objects, into key value pairs

--- src/back-end/menu_items.py
+++ src/back-end/menu_items.py
@@ -3,6 +3,14 @@ from database import Database
 import menu_items_class_sql as menu_items_table
 
 
+class RootObject:
+    def describe(self):
+        attrs = [a for a in dir(self) if not a.startswith('__')]
+        # for a in attrs:
+        #     if a != 'describe':
+        #         print(a+':', getattr(self, a))
+
+
 class MenuItems:
     def __init__(self):
         pass
@@ -17,7 +25,8 @@ class MenuItems:
         con, cur = db.open_database()
         cur.execute(menu_items_table.add_sql, data)
         con.commit()
-        result = {'count': cur.rowcount}
+        result = RootObject()
+        setattr(result, 'count', cur.rowcount)
         db.close_database()
         return result
 
@@ -42,8 +51,17 @@ class MenuItems:
         rows = cur.fetchall()
         result = {}
         if len(rows) == 1:
-            result['data_row'] = rows[0]
+            result = self.__get_row_with_column(rows[0], cur.description)
         db.close_database()
+        return result
+
+    @classmethod
+    def __get_row_with_column(self, row, cursor_description):
+        columns = list(map(lambda c: c[0], cursor_description))
+        result = RootObject()
+        for i in range(len(columns)):
+            setattr(result, columns[i], row[i])
+            # result[columns[i]] = row[i]
         return result
 
     @classmethod
