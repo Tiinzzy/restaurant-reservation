287fea7c44886b13aa500d719bacd369ce713864
Tina <tinzzy.cracked@yahoo.com>
2023-03-18 12:55:42 -0400

    modified table Creation sql

--- src/back-end/mysql_tables_class_sql.py
+++ src/back-end/mysql_tables_class_sql.py
@@ -67,7 +67,6 @@ users_sql = """
 create table tests.rr_user (
     id int NOT NULL AUTO_INCREMENT,
     name varchar(200),
-    lastName varchar(200),
     email varchar(200),
     password varchar(200),
     birthday varchar(200),
