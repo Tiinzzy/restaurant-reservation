2c5f6502db370f3384f0d2121231bbc05c733355
Tina <tinzzy.cracked@yahoo.com>
2023-03-20 09:48:28 -0400

    creating and object-oriented class for seating tables

---
+++ src/back-end/business/seating_tables.py
@@ -0,0 +1,28 @@
+from database import Database
+import business.users_class_sql as users_table
+
+
+class SeatingTables:
+
+    def __init__(self):
+        self.__id = None
+        self.__seat_count = None
+        self.__available = None
+
+    def get_id(self):
+        return self.__id
+
+    def set_seat_count(self, count):
+        self.__seat_count = count
+
+    def get_seat_count(self):
+        return self.__seat_count
+
+    def set_available(self, result):
+        self.__available = result
+
+    def get_available(self):
+        return self.__available
+
+    def to_string(self):
+        return str(self.__id) + ', ' + str(self.__seat_count) + ', ' + str(self.__available)
