3a0aedf2bbdbffddfb360bc5c2b84974bbb15b03
Tina <tinzzy.cracked@yahoo.com>
2023-03-20 12:53:48 -0400

    added class method to add menu items to the menu table

--- src/back-end/business/menu_items.py
+++ src/back-end/business/menu_items.py
@@ -1,3 +1,8 @@
+from database import Database
+
+import business.menu_items_class_sql as menu_items_table
+
+
 class MenuItems:
     def __init__(self):
         self.__id = None
@@ -41,6 +46,17 @@ class MenuItems:
 
     def get_description(self):
         return self.__description
+
+    def add(self):
+        data = (self.__name, self.__category, self.__price, self.__description)
+        db = Database()
+        con, cur = db.open_database()
+        cur.execute(menu_items_table.add_sql, data)
+        con.commit()
+        result = (cur.rowcount == 1)
+        self.__id = cur.lastrowid if cur.rowcount == 1 else -1
+        db.close_database()
+        return result
 
     def to_string(self):
         return str(self.__id) + ', ' + str(self.__name) + ', ' + str(self.__category) + ', ' + str(
