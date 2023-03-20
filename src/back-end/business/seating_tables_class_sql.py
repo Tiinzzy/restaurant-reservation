622e359a28828fcedbce87911e39eeac00c69168
Tina <tinzzy.cracked@yahoo.com>
2023-03-20 10:28:04 -0400

    sql command to select only available seats

--- src/back-end/business/seating_tables_class_sql.py
+++ src/back-end/business/seating_tables_class_sql.py
@@ -2,3 +2,9 @@ add_sql = """
 INSERT INTO tests.rr_seating_tables (seat_count, available)
     VALUES (%s,%s);
 """
+
+select_available_sql = """
+SELECT id FROM tests.rr_seating_tables
+    WHERE available = 'True' AND seat_count = %s ;
+"""
+
