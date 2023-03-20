a6d53fa58b6c63e5a0438e609be721640affb506
Tina <tinzzy.cracked@yahoo.com>
2023-03-20 10:14:40 -0400

    testing adding data into table in MySQL

--- src/back-end/tests/test_seating_tables_class.py
+++ src/back-end/tests/test_seating_tables_class.py
@@ -10,7 +10,7 @@ class TestUserClass(unittest.TestCase):
         self.assertEqual(s1.get_seat_count(), None)
         self.assertEqual(s1.get_available(), None)
 
-        print(s1.to_string())
+        # print(s1.to_string())
 
         s2 = SeatingTables()
         s2.set_seat_count(4)
@@ -18,11 +18,22 @@ class TestUserClass(unittest.TestCase):
 
         s2.set_available('False')
         self.assertEqual(s2.get_available(), 'False')
-        print(s2.to_string())
+        # print(s2.to_string())
 
     def test_build_seating(self):
         s1 = SeatingTables.build(2, 'True')
         self.assertEqual(s1.get_seat_count(), 2)
         self.assertEqual(s1.get_available(), 'True')
 
-        print(s1.to_string())
+        # print(s1.to_string())
+
+    def test_add_seating_tables(self):
+        s1 = SeatingTables.build(4, 'False')
+        self.assertEqual(s1.get_seat_count(), 4)
+        self.assertEqual(s1.get_available(), 'False')
+
+        s1.add()
+        self.assertTrue(s1.get_id() is not None)
+        self.assertTrue(s1.get_id() >= 0)
+
+        # print(s1.to_string())
