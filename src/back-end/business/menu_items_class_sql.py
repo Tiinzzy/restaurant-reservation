d3a0c41b45ba8a248e680c896b6e5f2fcae25836
Tina <tinzzy.cracked@yahoo.com>
2023-03-20 13:12:09 -0400

    added load and select and update sql command

--- src/back-end/business/menu_items_class_sql.py
+++ src/back-end/business/menu_items_class_sql.py
@@ -6,3 +6,15 @@ INSERT INTO tests.rr_menu_items (name, category, price, description)
 delete_sql = """
 DELETE FROM tests.rr_menu_items WHERE id = %s ; 
 """
+
+load_sql = """
+select * from tests.rr_menu_items;
+"""
+
+select_sql = """
+SELECT * FROM tests.rr_menu_items WHERE id = %s ; 
+"""
+
+update_sql = """
+UPDATE tests.rr_menu_items name = %s , category = %s , price = %s , description = %s ;
+"""
