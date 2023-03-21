9f8bdf3d95100351d3485a511930e8d203920c47
Tina <tinzzy.cracked@yahoo.com>
2023-03-21 10:10:47 -0400

    running test to get data from reservation works

--- src/back-end/tests/test_reservation_class.py
+++ src/back-end/tests/test_reservation_class.py
@@ -6,3 +6,18 @@ from business.reservation import Reservation
 class TestUserClass(unittest.TestCase):
     def test_new_reservation(self):
         r1 = Reservation()
+        self.assertEqual(r1.get_id(), None)
+        self.assertEqual(r1.get_timestamp(), None)
+        self.assertEqual(r1.get_customer_name(), None)
+        self.assertEqual(r1.get_customer_id(), None)
+        self.assertEqual(r1.get_seat_count(), None)
+        self.assertEqual(r1.get_table_id(), None)
+        self.assertEqual(r1.get_for_date(), None)
+        self.assertEqual(r1.get_for_how_long(), None)
+        self.assertEqual(r1.get_status(), None)
+        self.assertEqual(r1.get_latest_comment(), None)
+        self.assertEqual(r1.get_waiter_id(), None)
+        self.assertEqual(r1.get_reservation_type(), None)
+        self.assertEqual(r1.get_total_price(), None)
+        self.assertEqual(r1.get_tip_percent(), None)
+        print(r1.to_string())
