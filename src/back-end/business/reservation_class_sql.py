3d555e0c6caadb2ec764552477965929aff0d9a6
Tina <tinzzy.cracked@yahoo.com>
2023-03-21 11:17:38 -0400

    sql command to load a reservation based on  the id

--- src/back-end/business/reservation_class_sql.py
+++ src/back-end/business/reservation_class_sql.py
@@ -3,3 +3,7 @@ INSERT INTO tests.rr_reservation (timestamp, customer_name, customer_id, seat_co
     status, latest_comment, waiter_id, reservation_type, total_price, tip_percent)
     VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);
 """
+
+load_sql = """
+SELECT * FROM tests.rr_reservation WHERE id = %s ; 
+"""
