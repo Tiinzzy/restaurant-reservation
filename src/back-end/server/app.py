1ecc7edca355e13eebe47dc1d8c3c16dec13edb7
Tina <tinzzy.cracked@yahoo.com>
2023-03-22 17:35:18 -0400

    tested some of the apis and have to test more tomorrow

--- src/back-end/server/app.py
+++ src/back-end/server/app.py
@@ -4,15 +4,23 @@ from business.menu_item import MenuItem
 from business.seating_table import SeatingTable
 from business.reservation import Reservation
 
+import json
+import app_menu_item
+
 app = Flask(__name__)
+
+
+def get_parameters(req):
+    return json.loads(req.data.decode('utf8').replace("'", '"'))
 
 
 @app.route('/create-new-user-account', methods=["POST"])
 def create_new_user():
-    name = request.json['name']
-    email = request.json['email']
-    password = request.json['password']
-    birthday = request.json['birthday']
+    parameters = get_parameters(request)
+    name = parameters['name']
+    email = parameters['email']
+    password = parameters['password']
+    birthday = parameters['birthday']
 
     user = User()
     user.set_name(name)
@@ -52,10 +60,11 @@ def get_menu_items_categories():
 
 @app.route('/add-menu-item', methods=["POST"])
 def add_menu_item():
-    name = request.json['name']
-    category = request.json['category']
-    price = request.json['price']
-    description = request.json['description']
+    parameters = get_parameters(request)
+    name = parameters['name']
+    category = parameters['category']
+    price = parameters['price']
+    description = parameters['description']
 
     menu_item = MenuItem()
     menu_item.set_name(name)
@@ -75,7 +84,8 @@ def select_all_menu_items():
 
 @app.route('/delete-menu-item', methods=["POST"])
 def delete_menu_item():
-    menu_item_id = request.json['id']
+    parameters = get_parameters(request)
+    menu_item_id = parameters['id']
 
     menu_item = MenuItem()
     menu_item.load(menu_item_id)
@@ -86,30 +96,17 @@ def delete_menu_item():
 
 @app.route('/load-menu-item', methods=["POST"])
 def load_menu_item():
-    menu_item_id = request.json['id']
-
-    menu_item = MenuItem()
-    result = menu_item.load(menu_item_id)
-    return jsonify(result)
-
-
-@app.route('/update-menu-item', methods=["POST"])
-def update_menu_item():
-    menu_item_id = request.json['id']
-    name = request.json['name']
-    category = request.json['category']
-    price = request.json['price']
-    description = request.json['description']
-
+    parameters = get_parameters(request)
+    menu_item_id = parameters['id']
     menu_item = MenuItem()
     menu_item.load(menu_item_id)
+    return jsonify(menu_item.to_json())
 
-    menu_item.set_name(name)
-    menu_item.set_price(price)
-    menu_item.set_category(category)
-    menu_item.set_description(description)
 
-    result = menu_item.update()
+@app.route('/menu-item/update', methods=["POST"])
+def update_menu_item():
+    parameters = get_parameters(request)
+    result = app_menu_item.update(parameters)
     return jsonify(result)
 
 
