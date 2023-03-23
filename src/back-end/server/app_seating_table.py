3c61f0279ddc7c3d1efc8b1a5e2e2a1d029073e8
Tina <tinzzy.cracked@yahoo.com>
2023-03-23 18:25:38 -0400

    getting the values of objects

--- src/back-end/server/app_seating_table.py
+++ src/back-end/server/app_seating_table.py
@@ -2,23 +2,18 @@ from business.seating_table import SeatingTable
 
 
 def add(parameters):
-    seat_count = parameters['seat_count']
-    available = parameters['available']
-    if available == 'True':
-        available = True
-    else:
-        available = False
-
+    seat_count = parameters.get('seat_count')
+    available = (parameters.get('available') == 'True')
     seating_table = SeatingTable()
     seating_table.set_seat_count(seat_count)
     seating_table.set_available(available)
 
     result = seating_table.add()
-    return result
+    return {'result': result}
 
 
 def available_seats(parameters):
-    seat_count = parameters['seat_count']
+    seat_count = parameters.get('seat_count')
     result = SeatingTable.select_available_seats(int(seat_count))
     return result
 
@@ -29,19 +24,19 @@ def select_all():
 
 
 def load(parameters):
-    seat_id = parameters['id']
+    seat_id = parameters.get('id')
     table = SeatingTable()
     table.load(seat_id)
     return table.to_json()
 
 
 def update(parameters):
-    seat_id = parameters['id']
-    available = parameters['available']
+    seat_id = parameters.get('id')
+    available = parameters.get('available')
 
     table = SeatingTable()
     table.load(seat_id)
     table.set_available(available)
 
     result = table.update()
-    return result
+    return {'result': result}
