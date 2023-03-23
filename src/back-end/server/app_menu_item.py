bde30e22bc26c257813952460c255058caab03d2
Tina <tinzzy.cracked@yahoo.com>
2023-03-23 12:52:31 -0400

    api to have functions for calculating and getting data from database

--- src/back-end/server/app_menu_item.py
+++ src/back-end/server/app_menu_item.py
@@ -1,6 +1,39 @@
 from business.menu_item import MenuItem
 
 
+def add(parameters):
+    name = parameters['name']
+    category = parameters['category']
+    price = parameters['price']
+    description = parameters['description']
+
+    menu_item = MenuItem()
+    menu_item.set_name(name)
+    menu_item.set_price(price)
+    menu_item.set_category(category)
+    menu_item.set_description(description)
+
+    result = menu_item.add()
+    return result
+
+
+def delete(parameters):
+    menu_item_id = parameters['id']
+
+    menu_item = MenuItem()
+    menu_item.load(menu_item_id)
+
+    result = menu_item.delete()
+    return result
+
+
+def load(parameters):
+    menu_item_id = parameters['id']
+    menu_item = MenuItem()
+    menu_item.load(menu_item_id)
+    return menu_item.to_json()
+
+
 def update(parameters):
     menu_item_id = parameters['id']
     name = parameters['name']
@@ -18,3 +51,13 @@ def update(parameters):
 
     result = menu_item.update()
     return result
+
+
+def select_all():
+    menu_items = MenuItem.select_all_menu_items()
+    return menu_items
+
+
+def categories():
+    category = MenuItem.get_categories()
+    return category
