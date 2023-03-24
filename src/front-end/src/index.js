a4022087b8a34fc987d9a9e97def74c795ee5b1a
Tina <tinzzy.cracked@yahoo.com>
2023-03-24 17:18:11 -0400

    changed format of index.js to create react app

--- src/front-end/src/index.js
+++ src/front-end/src/index.js
@@ -1,8 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./App.scss";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
 
-const el = document.getElementById("app");
 
-ReactDOM.render(<App />, el);
+
+// import "./App.css";
+
+// const el = document.getElementById("app");
+
+// console.log(1212);
+
+// ReactDOM.render(<App />, el);
+
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+import "./App.css";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
