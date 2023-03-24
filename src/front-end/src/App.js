81911a573528c279b6ab3fe8c5c0e6b2b603b0ff
Tina <tinzzy.cracked@yahoo.com>
2023-03-24 17:17:31 -0400

    testing to see javascript works or not

--- src/front-end/src/App.js
+++ src/front-end/src/App.js
@@ -2,12 +2,21 @@ import { Box, Divider } from "@mui/material";
 import React from "react";
 
 
+var f = () => alert(1232345235);
+
 const App = () => {
     return (<>
         <Box p={5}>
             <Divider />
             Hello React, Now I am not using react-script any more
             <Divider />
+            <div className="divy" onClick={() => f()}>
+                YOU A CLICKER
+            </div>
+            <Divider />
+            <div className='newDiv'>
+                This is our key to get out of the home!!!! please!
+            </div>
         </Box>
     </>);
 };
