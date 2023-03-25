77ad3b2f7cb58e6e43833b4e0ca968510c53b5f3
Tina <tinzzy.cracked@yahoo.com>
2023-03-25 10:23:38 -0400

    changed style loader, reading index js file and also added proxy

--- src/front-end/webpack.config.js
+++ src/front-end/webpack.config.js
@@ -3,18 +3,29 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
     output: {
         path: path.join(__dirname, "/dist"), // the bundle output path
         filename: "bundle.js", // the name of the bundle
+        clean: true
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "src/index.html", // to import index.html file inside index.js
+            title: 'Development',
         }),
     ],
     devServer: {
         port: 3000, // you can change the port
-        open: true
+        open: true,
+        proxy: {
+            '/': {
+                target: 'http://localhost:3000',
+                router: () => 'http://localhost:5000',
+                logLevel: 'debug'
+            }
+        }
     },
     module: {
         rules: [
@@ -26,8 +37,8 @@ module.exports = {
                 },
             },
             {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                test: /\.(sa|sc|c)ss$/, // styles files
+                use: ["style-loader", "css-loader", "sass-loader"],
             },
             {
                 test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
