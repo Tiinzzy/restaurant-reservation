3a97e1766a6c383fbddadca66b6be07af04eb0a9
Tina <tina.vatanabadi@gmail.com>
2023-03-26 19:33:41 -0400

    added header components that would link to other pages

--- src/front-end/src/components/Header.js
+++ src/front-end/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Box from '@mui/material/Box';
+import Typography from "@mui/material/Typography";
 
 export default class Header extends React.Component {
 
@@ -12,8 +13,18 @@ export default class Header extends React.Component {
 
     render() {
         return (
-            <Box>
-                This will be Header
+            <Box className="header-box">
+                <Box className="header-box-left">
+                    <Typography id="links-typograpphy"> Menu </Typography>
+                    <Typography ml={3} id="links-typograpphy"> Reservations </Typography>
+                </Box>
+                <Box className="header-box-logo">
+                    <Typography fontSize="25px" id="links-typograpphy"> Restaurant Reservation </Typography>
+                </Box>
+                <Box className="header-box-right">
+                    <Typography mr={3} id="links-typograpphy"> Login </Typography>
+                    <Typography id="links-typograpphy"> Sign Up </Typography>
+                </Box>
             </Box>
         );
     }
