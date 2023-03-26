048c74fc23863543bc38d469c60cf42c8124af52
Tina <tina.vatanabadi@gmail.com>
2023-03-26 19:40:14 -0400

    added styling and dummy text

--- src/front-end/src/App.js
+++ src/front-end/src/App.js
@@ -8,6 +8,9 @@ export default function App() {
     return (
         <>
             <Header />
+            <div style={{ alignItems: 'center', justifyContent: 'center', display: 'flex', margin: '300px 0px 340px 0px' }}>
+                This is only a test and will be fixed later by adding other pages and data
+            </div>
             <Footer />
         </>
     );
