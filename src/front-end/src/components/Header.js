d13a3d34b737abc60b4c89453362fd13d3fb14a1
Tina <tina.vatanabadi@gmail.com>
2023-03-26 19:02:07 -0400

    created header component

---
+++ src/front-end/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Box from '@mui/material/Box';
+
+export default class Header extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <Box>
+                This will be Header
+            </Box>
+        );
+    }
+};
