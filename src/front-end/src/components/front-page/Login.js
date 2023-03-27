54066b760fbdb5bf54beb1306efe80a1b2d1c355
Tina <tina.vatanabadi@gmail.com>
2023-03-27 09:56:50 -0400

    component for login form

---
+++ src/front-end/src/components/front-page/Login.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Box from '@mui/material/Box';
+
+export default class Login extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <Box>
+                This is login page
+            </Box>
+        );
+    }
+};
