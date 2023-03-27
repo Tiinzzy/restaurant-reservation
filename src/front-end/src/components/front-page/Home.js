2136511d77b089494cc8017c7548284a234cd104
Tina <tina.vatanabadi@gmail.com>
2023-03-27 11:00:31 -0400

    added more details to home page

--- src/front-end/src/components/front-page/Home.js
+++ src/front-end/src/components/front-page/Home.js
@@ -20,8 +20,16 @@ export default class Home extends React.Component {
             <Box className="home-main-box">
                 <img src="https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2014/05/1399503143258.jpeg" height={550} />
                 <Box className="home-main-box-about-us">
-                <Typography fontFamily="serif" fontWeight="bold" fontSize="5em">Discover the World of Fine Dining</Typography>
-                {HOME_PAGE_DATA.map((e, i) => (<Typography key={i} fontFamily="serif" fontSize="1.1em" mt={2}>{e}</Typography>))}
+                    <Typography fontFamily="serif" fontWeight="bold" fontSize="5em" mb={2}>Discover the World of Fine Dining</Typography>
+                    {HOME_PAGE_DATA.map((e, i) => (<Typography key={i} fontFamily="serif" fontSize="1.1em">{e}</Typography>))}
+                    <Box className="home-main-box-buttons">
+                        <a href="#">
+                            Checkout Our Menu
+                        </a>
+                        <a href="#">
+                            Make reservation
+                        </a>
+                    </Box>
                 </Box>
             </Box>
         );
