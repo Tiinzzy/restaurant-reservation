1e3db275f5c45fd07a4238471e24d2fc053edde0
Tina <tina.vatanabadi@gmail.com>
2023-03-27 09:56:39 -0400

    component for displaying menu

---
+++ src/front-end/src/components/front-page/Menu.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Box from '@mui/material/Box';
+
+export default class Menu extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <Box>
+                This is menu page
+            </Box>
+        );
+    }
+};
