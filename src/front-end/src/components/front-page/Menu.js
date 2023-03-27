aae20a36dfb7c2ad5070cf8eebf44c4ddcb7a714
Tina <tina.vatanabadi@gmail.com>
2023-03-27 15:10:45 -0400

    added essential components of menu to later map data from database

--- src/front-end/src/components/front-page/Menu.js
+++ src/front-end/src/components/front-page/Menu.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Box from '@mui/material/Box';
+import Typography from "@mui/material/Typography";
 
 export default class Menu extends React.Component {
 
@@ -12,8 +13,20 @@ export default class Menu extends React.Component {
 
     render() {
         return (
-            <Box>
-                This is menu page
+            <Box className="menu-main-box">
+                <Box className="menu-box-menu-logo">
+                    <Typography fontFamily="serif" fontWeight="bold" fontSize="2em">Menu</Typography>
+                </Box>
+                <Box style={{display:'flex', justifyContent: 'space-evenly'}}>
+                    <Box className="menu-box-left-side">
+                    <Typography fontFamily="serif" fontWeight="bold" fontSize="1.2em">Entrée</Typography>
+                    <Typography fontFamily="serif" fontWeight="bold" fontSize="1.2em">Main</Typography>
+                    </Box>
+                    <Box className="menu-box-right-side">
+                    <Typography fontFamily="serif" fontWeight="bold" fontSize="1.2em">Drinks</Typography>
+                    <Typography fontFamily="serif" fontWeight="bold" fontSize="1.2em">Dessert</Typography>
+                    </Box>
+                </Box>
             </Box>
         );
     }
