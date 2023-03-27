7c4ebb4ef1905116910ffbace43830c2b0438de2
Tina <tina.vatanabadi@gmail.com>
2023-03-27 09:59:42 -0400

    component for home page

---
+++ src/front-end/src/components/front-page/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Box from '@mui/material/Box';
+
+export default class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <Box>
+                This is Home page
+            </Box>
+        );
+    }
+};
