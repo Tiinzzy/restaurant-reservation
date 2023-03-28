af19992bc789065214b4b47c39f9605a0777e5ea
Tina <tina.vatanabadi@gmail.com>
2023-03-28 16:36:19 -0400

    created a component as users home page

---
+++ src/front-end/src/components/user-page/UserHomePage.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+import BackEndConnection from '../backend-connection/BackEndConnection';
+
+const backend = BackEndConnection.INSTANCE();
+
+export default class UserHomePage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            showPassword: false,
+            user: props.user
+        }
+    }
+
+    logOutUser() {
+        backend.authentication_logout(this.state.user, (data) => {
+            console.log(data);
+        })
+    }
+
+
+    render() {
+        return (
+            <Box>
+                <Typography>This will be users home page.</Typography>
+                <Button variant="contained" onClick={() => this.logOutUser()}>LogOut</Button>
+            </Box>
+        );
+    }
+};
