78664d7f801700953024bb405a96f4db09328c27
Tina <tina.vatanabadi@gmail.com>
2023-03-28 16:37:02 -0400

    checking for login user and if is logged in take to their home page else not

--- src/front-end/src/App.js
+++ src/front-end/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -7,26 +8,55 @@ import Login from './components/front-page/Login';
 import Menu from './components/front-page/Menu';
 import Reservation from './components/front-page/Reservation';
 import Home from './components/front-page/Home';
+import UserHomePage from "./components/user-page/UserHomePage";
 
 import BackEndConnection from './components/backend-connection/BackEndConnection';
 
 const backend = BackEndConnection.INSTANCE();
 const CURRENT_PATH = window.location.pathname;
 
-export default function App() {
+function checkUserLogin(setIsLogin) {
+    backend.authentication_is_login((data) => {
+        if (data.is_login) {
+            setIsLogin(true);
+        }
+    })
+}
 
-    console.log(CURRENT_PATH, '<<<')
+export default function App(props) {
+    const [isLogin, setIsLogin] = useState(false);
+    const [user, setUser] = useState(null);
+
+    if(user !== null){
+        checkUserLogin(setIsLogin);
+    }
+
+    function callBack(user) {
+        setUser(user);
+    }
 
     return (
-        <>
-            <Header />
-            {(CURRENT_PATH === '/login') && <Login />}
-            {(CURRENT_PATH === '/' || CURRENT_PATH === '/home') && <Home />}
-            {(CURRENT_PATH === '/signup') && <Signup />}
-            {(CURRENT_PATH === '/menu') && <Menu />}
-            {(CURRENT_PATH === '/reservation') && <Reservation />}
-            <Footer />
-        </>
+        <div>
+            {isLogin ?
+                <>
+                    <Header />
+                    {(user !== null) && <UserHomePage user={user} />}
+                    <Footer />
+                </>
+                :
+                <>
+                    <Header />
+                    {(CURRENT_PATH === '/login') && <Login callBack={callBack} />}
+                    {(CURRENT_PATH === '/' || CURRENT_PATH === '/home') && <Home />}
+                    {(CURRENT_PATH === '/signup') && <Signup />}
+                    {(CURRENT_PATH === '/menu') && <Menu />}
+                    {(CURRENT_PATH === '/reservation') && <Reservation />}
+                    <Footer />
+
+                </>
+            }
+
+        </div>
     );
 };
 
