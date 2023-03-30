fa7607f545a04e224677bc3790b919684c03cd06
Tina <tina.vatanabadi@gmail.com>
2023-03-30 09:46:30 -0400

    added menu drop down for options available for user page

--- src/front-end/src/components/user-page/UserHomePage.js
+++ src/front-end/src/components/user-page/UserHomePage.js
@@ -2,7 +2,11 @@ import React from "react";
 
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Divider from '@mui/material/Divider';
 
 import './user-styling.scss';
 
@@ -10,15 +14,16 @@ import BackEndConnection from '../backend-connection/BackEndConnection';
 
 const backend = BackEndConnection.INSTANCE();
 
+const OPTIONS = ['Make Reservation', 'Change Password', 'Edit Profile', 'Reservation History'];
+
 export default class UserHomePage extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            email: '',
-            password: '',
-            showPassword: false,
-            user: props.user
+            user: props.user,
+            openMenu: false,
+            anchorEl: null
         }
     }
 
@@ -30,13 +35,47 @@ export default class UserHomePage extends React.Component {
         })
     }
 
+    openMenuDetails(e) {
+        this.setState({ openMenu: true, anchorEl: e.currentTarget });
+    }
+
+    closeMenuDetails() {
+        this.setState({ openMenu: false, anchorEl: null });
+    }
 
     render() {
         return (
             <Box className="user-home-page-main-box">
                 <Box className="user-home-page-data-box">
-                    <Typography>This will be users home page. test</Typography>
-                    <Button variant="contained" onClick={() => this.logOutUser()}>LogOut</Button>
+                    <Box className="user-home-page-users-name">
+                        <Typography>Hi {this.state.user}, Welcome Back!</Typography>
+                        <Box display="flex" flexGrow={1} />
+                        <IconButton color="primary" aria-label="menu">
+                            <MenuIcon fontSize="large" onClick={(e) => this.openMenuDetails(e)} />
+                        </IconButton>
+
+                        <Menu
+                            anchorEl={this.state.anchorEl}
+                            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                            open={this.state.openMenu}
+                            onClose={() => this.closeMenuDetails()}
+                            PaperProps={{
+                                style: {
+                                    maxHeight: 100 * 4.5,
+                                    width: '20ch',
+                                },
+                            }}>
+                            {OPTIONS.map((e, i) => (
+                                <MenuItem key={i} onClick={() => this.closeMenuDetails()}>
+                                    {e}
+                                    <Divider />
+                                </MenuItem>
+                            ))}
+                        </Menu>
+
+                    </Box>
+
                 </Box>
             </Box>
         );
