9a2dfa8ff674ca67af0bd812fd101cad16f40710
Tina <tina.vatanabadi@gmail.com>
2023-03-31 10:47:09 -0400

    fixed the adding menu items side of the page

--- src/front-end/src/components/admin-page/EditMenu.js
+++ src/front-end/src/components/admin-page/EditMenu.js
@@ -3,6 +3,8 @@ import React from "react";
 import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 import BackEndConnection from '../backend-connection/BackEndConnection';
 
@@ -25,6 +27,28 @@ export default class EditMenu extends React.Component {
                     <Box display="flex" flexGrow={1} />
                 </Box>
                 <Divider style={{ margingTop: 10, marginBottom: 25 }} />
+                <Box style={{ display: 'flex', flexDirection: 'row', marginBottom: 40 }}>
+                    <Box className="user-page-reservation-form-1">
+                        <Typography fontSize={14} variant="body1" mb={.5}>Name: </Typography>
+                        <TextField variant="outlined" className="localization-provider"
+                            onChange={(e) => this.getFoodName(e)} />
+                        <Typography fontSize={14} variant="body1" mb={.5}>Category: </Typography>
+                        <TextField variant="outlined" className="localization-provider"
+                            onChange={(e) => this.getCategory(e)} />
+                        <Typography fontSize={14} variant="body1" mb={.5}>Price: </Typography>
+                        <TextField variant="outlined" className="localization-provider"
+                            onChange={(e) => this.getPrice(e)} />
+                        <Typography fontSize={14} variant="body1" mb={.5}>Description: </Typography>
+                        <TextField variant="outlined" className="localization-provider"
+                            onChange={(e) => this.getDescription(e)} />
+                        <Button onClick={() => this.createNewUserAccount()} variant="contained">Submit</Button>
+
+                    </Box>
+                    <Box display="flex" flexGrow={1} />
+                    <Box className="user-page-reservation-form">
+                        this is for editing pre-existing menu items
+                    </Box>
+                </Box>
             </Box>
         );
     }
