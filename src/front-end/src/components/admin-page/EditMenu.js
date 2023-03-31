d937038da0d138d823eaa5ce6bbd6c1248b5df70
Tina <tina.vatanabadi@gmail.com>
2023-03-31 11:07:35 -0400

    snackbar opens to inform admin that menu item has been added successfully

--- src/front-end/src/components/admin-page/EditMenu.js
+++ src/front-end/src/components/admin-page/EditMenu.js
@@ -5,6 +5,8 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Alert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
 
 import BackEndConnection from '../backend-connection/BackEndConnection';
 
@@ -18,7 +20,9 @@ export default class EditMenu extends React.Component {
             foodName: '',
             category: '',
             price: '',
-            description: ''
+            description: '',
+            addItem: false,
+            openSnack: false
         }
     }
 
@@ -46,41 +50,56 @@ export default class EditMenu extends React.Component {
 
     submitAddingMenuItem() {
         backend.add_menu_item(this.state.foodName, this.state.category, this.state.price, this.state.description, (data) => {
-            console.log(data);
+            let that = this;
+            if (data.result === true) {
+                that.setState({ addItem: true, openSnack:true });
+            }
         })
+    }
+
+    closeAlert() {
+        this.setState({ openSnack: false });
     }
 
     render() {
         return (
-            <Box className="delete-account-main-box">
-                <Box className="top-header-reservation">
-                    <Typography fontSize={20} fontWeight="bold">Edit Menu</Typography>
-                    <Box display="flex" flexGrow={1} />
-                </Box>
-                <Divider style={{ margingTop: 10, marginBottom: 25 }} />
-                <Box style={{ display: 'flex', flexDirection: 'row', marginBottom: 40 }}>
-                    <Box className="user-page-reservation-form-1">
-                        <Typography fontSize={14} variant="body1" mb={.5}>Name: </Typography>
-                        <TextField variant="outlined" className="localization-provider"
-                            onChange={(e) => this.getFoodName(e)} />
-                        <Typography fontSize={14} variant="body1" mb={.5}>Category: </Typography>
-                        <TextField variant="outlined" className="localization-provider"
-                            onChange={(e) => this.getCategory(e)} />
-                        <Typography fontSize={14} variant="body1" mb={.5}>Price: </Typography>
-                        <TextField variant="outlined" className="localization-provider"
-                            onChange={(e) => this.getPrice(e)} />
-                        <Typography fontSize={14} variant="body1" mb={.5}>Description: </Typography>
-                        <TextField variant="outlined" className="localization-provider"
-                            onChange={(e) => this.getDescription(e)} />
-                        <Button onClick={() => this.submitAddingMenuItem()} variant="contained">Submit</Button>
+            <>
+                <Box className="delete-account-main-box">
+                    <Box className="top-header-reservation">
+                        <Typography fontSize={20} fontWeight="bold">Edit Menu</Typography>
+                        <Box display="flex" flexGrow={1} />
+                    </Box>
+                    <Divider style={{ margingTop: 10, marginBottom: 25 }} />
+                    <Box style={{ display: 'flex', flexDirection: 'row', marginBottom: 40 }}>
+                        <Box className="user-page-reservation-form-1">
+                            <Typography fontSize={14} variant="body1" mb={.5}>Name: </Typography>
+                            <TextField variant="outlined" className="localization-provider"
+                                onChange={(e) => this.getFoodName(e)} />
+                            <Typography fontSize={14} variant="body1" mb={.5}>Category: </Typography>
+                            <TextField variant="outlined" className="localization-provider"
+                                onChange={(e) => this.getCategory(e)} />
+                            <Typography fontSize={14} variant="body1" mb={.5}>Price: </Typography>
+                            <TextField variant="outlined" className="localization-provider"
+                                onChange={(e) => this.getPrice(e)} />
+                            <Typography fontSize={14} variant="body1" mb={.5}>Description: </Typography>
+                            <TextField variant="outlined" className="localization-provider"
+                                onChange={(e) => this.getDescription(e)} />
+                            <Button onClick={() => this.submitAddingMenuItem()} variant="contained">Submit</Button>
 
-                    </Box>
-                    <Box display="flex" flexGrow={1} />
-                    <Box className="user-page-reservation-form">
-                        this is for editing pre-existing menu items
+                        </Box>
+                        <Box display="flex" flexGrow={1} />
+                        <Box className="user-page-reservation-form">
+                            this is for editing pre-existing menu items
+                        </Box>
                     </Box>
                 </Box>
-            </Box>
+                {this.state.addItem === true &&
+                    <Snackbar open={this.state.openSnack} onClose={() => this.closeAlert()} autoHideDuration={5000} anchorOrigin={{ vertical: "top", horizontal: "center" }}>
+                        <Alert severity="success">
+                            Menu Item Added Successfully!
+                        </Alert>
+                    </Snackbar>}
+            </>
         );
     }
 };
