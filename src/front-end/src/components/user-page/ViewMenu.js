c35d5de110d9d26d5d366a300fa949d2f74c64a5
Tina <tina.vatanabadi@gmail.com>
2023-04-02 18:54:41 -0400

    added details of menu and their menu items to the page

--- src/front-end/src/components/user-page/ViewMenu.js
+++ src/front-end/src/components/user-page/ViewMenu.js
@@ -51,6 +51,57 @@ export default class ViewMenu extends React.Component {
                     <Typography fontSize={16} variant="body1">You can brows our latest menu options.</Typography>
                 </Box>
                 <Divider style={{ margingTop: 10, marginBottom: 25 }} />
+
+                <Box style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+                    <Box className="menu-box-left-side-user">
+                        <Typography fontFamily="serif" fontWeight="bold" fontSize="1.3em" mb={1.6} mt={1.6}>Entrée</Typography>
+                        {this.state.entrees && this.state.entrees.map((e, i) => (
+                            <Box key={i} className="each-menu-item-box">
+                                <span style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-betwee' }}>
+                                    <Typography fontFamily="serif" fontWeight="bold" fontSize="1em" color="rgb(37, 37, 37)">{e.name}</Typography>
+                                    <Box display="flex" flexGrow={1} />
+                                    <Typography fontFamily="serif" fontWeight="600" fontSize=".9em" color="rgb(37, 37, 37)">${e.price}</Typography>
+                                </span>
+                                <Typography fontFamily="serif" fontSize=".8em" color="rgb(94, 94, 94)">{e.description}</Typography>
+                            </Box>
+                        ))}
+                        <Typography fontFamily="serif" fontWeight="bold" fontSize="1.3em" mb={1.6} mt={1.6}>Main Course</Typography>
+                        {this.state.mainCourse && this.state.mainCourse.map((e, i) => (
+                            <Box key={i} className="each-menu-item-box">
+                                <span style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-betwee' }}>
+                                    <Typography fontFamily="serif" fontWeight="bold" fontSize="1em" color="rgb(37, 37, 37)">{e.name}</Typography>
+                                    <Box display="flex" flexGrow={1} />
+                                    <Typography fontFamily="serif" fontWeight="600" fontSize=".9em" color="rgb(37, 37, 37)">${e.price}</Typography>
+                                </span>
+                                <Typography fontFamily="serif" fontSize=".8em" color="rgb(94, 94, 94)">{e.description}</Typography>
+                            </Box>
+                        ))}
+                    </Box>
+                    <Box className="menu-box-right-side-user">
+                        <Typography fontFamily="serif" fontWeight="bold" fontSize="1.3em" mb={1.6} mt={1.6}>Drinks</Typography>
+                        {this.state.drinks && this.state.drinks.map((e, i) => (
+                            <Box key={i} className="each-menu-item-box">
+                                <span style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-betwee' }}>
+                                    <Typography fontFamily="serif" fontWeight="bold" fontSize="1em" color="rgb(37, 37, 37)">{e.name}</Typography>
+                                    <Box display="flex" flexGrow={1} />
+                                    <Typography fontFamily="serif" fontWeight="600" fontSize=".9em" color="rgb(37, 37, 37)">${e.price}</Typography>
+                                </span>
+                                <Typography fontFamily="serif" fontSize=".8em" color="rgb(94, 94, 94)">{e.description}</Typography>
+                            </Box>
+                        ))}
+                        <Typography fontFamily="serif" fontWeight="bold" fontSize="1.3em" mb={1.6} mt={1.6}>Dessert</Typography>
+                        {this.state.dessert && this.state.dessert.map((e, i) => (
+                            <Box key={i} className="each-menu-item-box">
+                                <span style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-betwee' }}>
+                                    <Typography fontFamily="serif" fontWeight="bold" fontSize="1em" color="rgb(37, 37, 37)">{e.name}</Typography>
+                                    <Box display="flex" flexGrow={1} />
+                                    <Typography fontFamily="serif" fontWeight="600" fontSize=".9em" color="rgb(37, 37, 37)">${e.price}</Typography>
+                                </span>
+                                <Typography fontFamily="serif" fontSize=".8em" color="rgb(94, 94, 94)">{e.description}</Typography>
+                            </Box>
+                        ))}
+                    </Box>
+                </Box>
             </Box>
         );
     }
