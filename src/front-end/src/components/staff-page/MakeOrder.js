549c3bf50fa2ae8d682274f8b630252099f578f4
Tina <tina.vatanabadi@gmail.com>
2023-04-03 11:55:20 -0400

    added menu items for staff with a plus icon to add order based on the customers need

--- src/front-end/src/components/staff-page/MakeOrder.js
+++ src/front-end/src/components/staff-page/MakeOrder.js
@@ -3,6 +3,8 @@ import React from "react";
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 import BackEndConnection from '../backend-connection/BackEndConnection';
 
@@ -16,6 +18,13 @@ export default class MakeOrder extends React.Component {
         }
     }
 
+    componentDidMount() {
+        backend.all_menu_items((data) => {
+            let that = this;
+            that.setState({ data });
+        })
+    }
+
     render() {
         return (
             <Box className="delete-account-main-box">
@@ -25,6 +34,24 @@ export default class MakeOrder extends React.Component {
                     <Typography fontSize={16} variant="body1">To make an order, just click on the + to add.</Typography>
                 </Box>
                 <Divider style={{ margingTop: 10, marginBottom: 25 }} />
+                <Box className="user-page-reservation-form-4">
+                    {this.state.data && this.state.data.map((e, i) => (
+                        <Box key={i}>
+                            <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-betwee', alignItems: 'center' }}>
+                            <Typography fontFamily="serif" fontWeight="bold" fontSize="1em" color="rgb(37, 37, 37)">{e.name}</Typography>
+                                <Box display="flex" flexGrow={1} />
+                                <span>
+                                <span fontFamily="serif" color="rgb(37, 37, 37)">${e.price}</span>
+                                    <IconButton aria-label="add">
+                                        <AddCircleOutlineIcon />
+                                    </IconButton>
+                                </span>
+                            </Box>
+                            <br />
+                            <Typography fontFamily="serif" fontSize=".8em" color="rgb(94, 94, 94)">{e.description}</Typography>
+                            <Divider />
+                        </Box>))}
+                </Box>
             </Box>
         );
     }
