a703b30a9dbfbdf319be91539e5079cc5c843ffa
Tina <tina.vatanabadi@gmail.com>
2023-04-04 14:14:46 -0400

    added alert and snackbar on successful addition of reservation

--- src/front-end/src/components/staff-page/MakeReservation.js
+++ src/front-end/src/components/staff-page/MakeReservation.js
@@ -8,6 +8,8 @@ import Button from '@mui/material/Button';
 import FormControl from '@mui/material/FormControl';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
+import Alert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
@@ -42,7 +44,10 @@ export default class MakeReservation extends React.Component {
             totalPrice: '',
             waiterId: '',
             statusType: 'Just walked in',
-            reservationType: 'Phone'
+            reservationType: 'Phone',
+            reservationSuccess: false,
+            openSnack: false,
+            reserveErr: false
         }
     }
 
@@ -52,7 +57,7 @@ export default class MakeReservation extends React.Component {
     }
 
     setReserveTime(e) {
-        let resTime = e.$H + '/' + e.$m;
+        let resTime = e.$H + ':' + e.$m;
         this.setState({ reservationTime: resTime });
     }
 
@@ -107,106 +112,123 @@ export default class MakeReservation extends React.Component {
     submitReservation() {
         let query = {
             'timestamp': this.state.reservationTime, 'customer_name': this.state.customerFullName, 'customer_id': this.state.customerId, 'seat_count': this.state.numberOfPeople, 'table_id': this.state.tableId, 'for_date': this.state.reservation,
-            'for_how_long': this.state.duration, 'status': this.state.status, 'latest_comment': this.state.latestComment, 'waiter_id': this.state.waiterId, 'reservation_type': this.state.reservationType, 'total_price': this.state.totalPrice,
+            'for_how_long': this.state.duration, 'status': this.state.statusType, 'latest_comment': this.state.latestComment, 'waiter_id': this.state.waiterId, 'reservation_type': this.state.reservationType, 'total_price': this.state.totalPrice,
             'tip_percent': this.state.tipPercent
         };
 
         backend.add_reservation(query, (data) => {
-            console.log(data);
+            let that = this;
+            if (data.result) {
+                that.setState({ reservationSuccess: true, openSnack: true });
+            } else {
+                that.setState({ reserveErr: true });
+            }
         })
+    }
+
+    closeAlert() {
+        this.setState({ openSnack: false });
     }
 
 
     render() {
         return (
-            <Box className="delete-account-main-box">
-                <Box className="top-header-reservation">
-                    <Typography fontSize={20} fontWeight="bold">Make Reservation</Typography>
-                    <Box display="flex" flexGrow={1} />
-                    <Typography fontSize={16} variant="body1">To make a reservation please fill out the form bellow.</Typography>
-                </Box>
-                <Divider style={{ margingTop: 10, marginBottom: 25 }} />
-                <Box style={{ display: 'flex', flexDirection: 'row', marginBottom: 40, overflowY: 'scroll' }}>
-                    <Box className="user-page-reservation-form-1">
-                        <Typography fontSize={14} variant="body1" mb={.5}>Date: </Typography>
-                        <LocalizationProvider dateAdapter={AdapterDayjs}>
-                            <DatePicker className="localization-provider" value={this.state.reserveDate} onChange={(newValue) => this.getReserveDate(newValue)} format="DD-MM-YYYY" views={["year", "month", "day"]} />
-                        </LocalizationProvider>
-                        <Typography fontSize={14} variant="body1" mb={.5}>Time: </Typography>
-                        <LocalizationProvider dateAdapter={AdapterDayjs}>
-                            <TimePicker
-                                className="localization-provider"
-                                value={this.state.time}
-                                onChange={(newValue) => {
-                                    this.setState({ time: newValue }, () => {
-                                        this.setReserveTime(newValue)
-                                    })
-                                }}
-                            />
-                        </LocalizationProvider>
-                        <Typography fontSize={14} variant="body1" mb={.5}>Duration: </Typography>
-                        <TextField variant="outlined" className="localization-provider"
-                            onChange={(e) => this.getDurationOfStay(e)} />
-                        <Typography fontSize={14} variant="body1" mb={.5}>Number of People: </Typography>
-                        <FormControl>
-                            <Select
-                                className="localization-provider"
-                                value={this.state.numberOfPeople}
-                                onChange={(e) => this.handleOpenMenu(e)}>
-                                {NUMBER_OF_PEOPLE.map((e, i) => (
-                                    <MenuItem key={i} value={e}>{e}</MenuItem>))}
-                            </Select>
-                        </FormControl>
-                        <Typography fontSize={14} variant="body1" mb={.5}>Table ID: </Typography>
-                        <TextField type="text" inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} variant="outlined" className="localization-provider"
-                            onChange={(e) => this.getTableId(e)} />
-                        <Typography fontSize={14} variant="body1" mb={.5}>Waiter ID: </Typography>
-                        <TextField type="text" inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} variant="outlined" className="localization-provider"
-                            onChange={(e) => this.getWaiterId(e)} />
-                        <Typography fontSize={14} variant="body1" mb={.5}>Total Price: </Typography>
-                        <TextField type="text" inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} variant="outlined" className="localization-provider"
-                            onChange={(e) => this.getTotalPrice(e)} />
-                        <Typography fontSize={14} variant="body1" mb={.5}>Tip Percent: </Typography>
-                        <TextField type="text" inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} variant="outlined" className="localization-provider"
-                            onChange={(e) => this.getTipPercent(e)} />
+            <>
+                <Box className="delete-account-main-box">
+                    <Box className="top-header-reservation">
+                        <Typography fontSize={20} fontWeight="bold">Make Reservation</Typography>
+                        <Box display="flex" flexGrow={1} />
+                        <Typography fontSize={16} variant="body1">To make a reservation please fill out the form bellow.</Typography>
                     </Box>
-                    <Box display="flex" flexGrow={1} />
-                    <Box className="user-page-reservation-form">
-                        <Typography fontSize={14} variant="body1" mb={.5}>Customer Full Name: </Typography>
-                        <TextField variant="outlined" className="localization-provider"
-                            onChange={(e) => this.getCustomerFullName(e)} />
-                        <Typography fontSize={14} variant="body1" mb={.5}>Customer ID: </Typography>
-                        <TextField type="text" inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} variant="outlined" className="localization-provider"
-                            onChange={(e) => this.getCustomerId(e)} />
-                        <Typography fontSize={14} variant="body1" mb={.5}>Status: </Typography>
-                        <FormControl>
-                            <Select
-                                className="localization-provider"
-                                value={this.state.statusType}
-                                onChange={(e) => this.handleOpenStatusType(e)}>
-                                {STATUS_TYPE.map((e, i) => (
-                                    <MenuItem key={i} value={e}>{e}</MenuItem>))}
-                            </Select>
-                        </FormControl>
-                        <Typography fontSize={14} variant="body1" mb={.5}>Latest Comment: </Typography>
-                        <TextField variant="outlined" className="localization-provider"
-                            onChange={(e) => this.getLatestComment(e)} />
-                        <Typography fontSize={14} variant="body1" mb={.5}>Reservation Type: </Typography>
-                        <FormControl>
-                            <Select
-                                className="localization-provider"
-                                value={this.state.reservationType}
-                                onChange={(e) => this.handleOpenReservationType(e)}>
-                                {RESERVATION_TYPE.map((e, i) => (
-                                    <MenuItem key={i} value={e}>{e}</MenuItem>))}
-                            </Select>
-                        </FormControl>
-                        <Box style={{ display: 'flex', flexDirection: 'row', alignItems: 'right', justifyContent: 'right', marginTop: 40 }}>
-                            < Button onClick={() => this.submitReservation()} variant="contained" className="user-page-submit-bt-4">Submit</Button>
+                    <Divider style={{ margingTop: 10, marginBottom: 25 }} />
+                    <Box style={{ display: 'flex', flexDirection: 'row', marginBottom: 40, overflowY: 'scroll' }}>
+                        <Box className="user-page-reservation-form-1">
+                            <Typography fontSize={14} variant="body1" mb={.5}>Date: </Typography>
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <DatePicker className="localization-provider" value={this.state.reserveDate} onChange={(newValue) => this.getReserveDate(newValue)} format="DD-MM-YYYY" views={["year", "month", "day"]} />
+                            </LocalizationProvider>
+                            <Typography fontSize={14} variant="body1" mb={.5}>Time: </Typography>
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                <TimePicker
+                                    className="localization-provider"
+                                    value={this.state.time}
+                                    onChange={(newValue) => {
+                                        this.setState({ time: newValue }, () => {
+                                            this.setReserveTime(newValue)
+                                        })
+                                    }}
+                                />
+                            </LocalizationProvider>
+                            <Typography fontSize={14} variant="body1" mb={.5}>Duration: </Typography>
+                            <TextField variant="outlined" className="localization-provider"
+                                onChange={(e) => this.getDurationOfStay(e)} />
+                            <Typography fontSize={14} variant="body1" mb={.5}>Number of People: </Typography>
+                            <FormControl>
+                                <Select
+                                    className="localization-provider"
+                                    value={this.state.numberOfPeople}
+                                    onChange={(e) => this.handleOpenMenu(e)}>
+                                    {NUMBER_OF_PEOPLE.map((e, i) => (
+                                        <MenuItem key={i} value={e}>{e}</MenuItem>))}
+                                </Select>
+                            </FormControl>
+                            <Typography fontSize={14} variant="body1" mb={.5}>Table ID: </Typography>
+                            <TextField type="text" inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} variant="outlined" className="localization-provider"
+                                onChange={(e) => this.getTableId(e)} />
+                            <Typography fontSize={14} variant="body1" mb={.5}>Waiter ID: </Typography>
+                            <TextField type="text" inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} variant="outlined" className="localization-provider"
+                                onChange={(e) => this.getWaiterId(e)} />
+                            <Typography fontSize={14} variant="body1" mb={.5}>Total Price: </Typography>
+                            <TextField type="text" inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} variant="outlined" className="localization-provider"
+                                onChange={(e) => this.getTotalPrice(e)} />
+                            <Typography fontSize={14} variant="body1" mb={.5}>Tip Percent: </Typography>
+                            <TextField type="text" inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} variant="outlined" className="localization-provider"
+                                onChange={(e) => this.getTipPercent(e)} />
+                        </Box>
+                        <Box display="flex" flexGrow={1} />
+                        <Box className="user-page-reservation-form">
+                            <Typography fontSize={14} variant="body1" mb={.5}>Customer Full Name: </Typography>
+                            <TextField variant="outlined" className="localization-provider"
+                                onChange={(e) => this.getCustomerFullName(e)} />
+                            <Typography fontSize={14} variant="body1" mb={.5}>Customer ID: </Typography>
+                            <TextField type="text" inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} variant="outlined" className="localization-provider"
+                                onChange={(e) => this.getCustomerId(e)} />
+                            <Typography fontSize={14} variant="body1" mb={.5}>Status: </Typography>
+                            <FormControl>
+                                <Select
+                                    className="localization-provider"
+                                    value={this.state.statusType}
+                                    onChange={(e) => this.handleOpenStatusType(e)}>
+                                    {STATUS_TYPE.map((e, i) => (
+                                        <MenuItem key={i} value={e}>{e}</MenuItem>))}
+                                </Select>
+                            </FormControl>
+                            <Typography fontSize={14} variant="body1" mb={.5}>Latest Comment: </Typography>
+                            <TextField variant="outlined" className="localization-provider"
+                                onChange={(e) => this.getLatestComment(e)} />
+                            <Typography fontSize={14} variant="body1" mb={.5}>Reservation Type: </Typography>
+                            <FormControl>
+                                <Select
+                                    className="localization-provider"
+                                    value={this.state.reservationType}
+                                    onChange={(e) => this.handleOpenReservationType(e)}>
+                                    {RESERVATION_TYPE.map((e, i) => (
+                                        <MenuItem key={i} value={e}>{e}</MenuItem>))}
+                                </Select>
+                            </FormControl>
+                            <Box style={{ display: 'flex', flexDirection: 'row', alignItems: 'right', justifyContent: 'right', marginTop: 40 }}>
+                                < Button onClick={() => this.submitReservation()} variant="contained" className="user-page-submit-bt-4">Submit</Button>
+                            </Box>
                         </Box>
                     </Box>
                 </Box>
-            </Box>
+                {this.state.reservationSuccess === true &&
+                    <Snackbar open={this.state.openSnack} onClose={() => this.closeAlert()} autoHideDuration={5000} anchorOrigin={{ vertical: "top", horizontal: "center" }}>
+                        <Alert severity={this.state.reserveErr === true ? "error" : "success"}>
+                            {this.state.reserveErr === true ? "Sorry, Something went wrong!" : "Reservation Made Successfully!"}
+                        </Alert>
+                    </Snackbar>}
+            </>
         );
     }
 };
