a9370ec503411733546d08adb068d6450f34e95d
Tina <tina.vatanabadi@gmail.com>
2023-04-05 10:18:14 -0400

    created component for reservation detail and edit dialog

---
+++ src/front-end/src/components/staff-page/EditReservationDialog.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Box from '@mui/material/Box';
+
+import BackEndConnection from '../backend-connection/BackEndConnection';
+
+const backend = BackEndConnection.INSTANCE();
+
+export default class EditMenuDialog extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <Box>
+               this is to display data for one reservation
+            </Box>
+        );
+    }
+};
