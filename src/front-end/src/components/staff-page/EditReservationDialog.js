f6cdfa7e6f167553c8b15e08ec59bce02f828301
Tina <tina.vatanabadi@gmail.com>
2023-04-05 13:31:55 -0400

    adding dialog components for editing the values on open

--- src/front-end/src/components/staff-page/EditReservationDialog.js
+++ src/front-end/src/components/staff-page/EditReservationDialog.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 
 import BackEndConnection from '../backend-connection/BackEndConnection';
 
@@ -18,14 +25,41 @@ export default class EditMenuDialog extends React.Component {
 
     componentDidMount() {
         backend.load_reservation(this.state.reservationId, (data) => {
-            console.log(data);
+            let that = this;
+            that.setState({ data });
         })
+    }
+
+    cancelAndClose() {
+        this.state.closeDialog();
+    }
+
+    saveChanges() {
+        this.state.closeDialog();
     }
 
     render() {
         return (
             <Box>
-                this is to display data for one reservation
+                <DialogTitle id="alert-dialog-title">
+                    {"Edit Reservation #" + this.state.reservationId}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        The following are current data, type in textfield to change.
+                    </DialogContentText>
+                    <Typography fontWeight='bold' fontSize={14} variant="body1" mb={.5} mt={1.5}>Name: </Typography>
+                    {this.state.data && this.state.data.map((e, i) => (
+                        <Box key={i} >
+                        {e.id}
+                        </Box>))}
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => this.cancelAndClose()}>Cancel</Button>
+                    <Button onClick={() => this.saveChanges()} autoFocus>
+                        Save Changes
+                    </Button>
+                </DialogActions>
             </Box>
         );
     }
