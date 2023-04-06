0084b6080f19e187f610d6e17037a6832a808c5b
Tina <tina.vatanabadi@gmail.com>
2023-04-06 09:24:10 -0400

    api to update a reservation and make changes

--- src/front-end/src/components/backend-connection/BackEndConnection.js
+++ src/front-end/src/components/backend-connection/BackEndConnection.js
@@ -164,6 +164,20 @@ class BackEndConnectionImpl {
                 return false;
             })
     }
+
+    async update_reservation(callback) {
+        return axios.post('api/reservation/update', {}, {})
+            .then(function (response) {
+                if (callback) {
+                    callback(response.data);
+                }
+                return response.data
+            })
+            .catch(function (error) {
+                console.log(error);
+                return false;
+            })
+    }
 }
 
 export default class BackEndConnection {
