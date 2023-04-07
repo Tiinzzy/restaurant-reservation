a90ef1e41ff6831c35b6bc8d304fbae619eaa667
Tina <tina.vatanabadi@gmail.com>
2023-04-07 09:13:52 -0400

    fixed the display inside the dialog, still need to fix the buttons

--- src/front-end/src/components/staff-page/EditOrderDialog.js
+++ src/front-end/src/components/staff-page/EditOrderDialog.js
@@ -1,6 +1,15 @@
 import React from "react";
 
 import Box from '@mui/material/Box';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import Button from '@mui/material/Button';
 
 import BackEndConnection from '../backend-connection/BackEndConnection';
 
@@ -16,13 +25,44 @@ export default class EditOrderDialog extends React.Component {
     }
 
     componentDidMount() {
-        console.log(this.state.clickedData)
+        this.setState({ reservationId: this.state.clickedData.reservationId, orderItemId: this.state.clickedData.orderItemId, count: this.state.clickedData.count });
     }
 
     render() {
         return (
             <Box>
-                This will be dialog for editing order item
+                <DialogTitle id="alert-dialog-title">
+                    {"Edit Order"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        To delete or change the count.
+                    </DialogContentText>
+                    <Typography style={{ display: 'flex', justifyContent: 'space-between' }}
+                        fontWeight='bold' fontSize={14} variant="body1" mb={.5} mt={1.5}>Order Item Id:
+                        <span>
+                            {this.state.orderItemId}
+                        </span>
+                    </Typography>
+                    <Typography style={{ display: 'flex', justifyContent: 'space-between' }}
+                        fontWeight='bold' fontSize={14} variant="body1" mb={.5} mt={1.5}>Count:
+                        <span>
+                            <IconButton style={{ marginRight: 10 }} onClick={() => this.reduceTheCount()}>
+                                <RemoveCircleOutlineIcon />
+                            </IconButton>
+                            {this.state.count}
+                            <IconButton style={{ marginLeft: 10 }} onClick={() => this.increaseTheCount()}>
+                                <AddCircleOutlineIcon />
+                            </IconButton>
+                        </span>
+                    </Typography>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => this.cancelAndClose()}>Cancel</Button>
+                    <Button onClick={() => this.saveChanges()} autoFocus>
+                        Save Changes
+                    </Button>
+                </DialogActions>
             </Box>
         );
     }
