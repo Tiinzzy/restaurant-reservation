3e72fb072342d50cd264f70c711cfe0e9d8aaf52
Tina <tinzzy.cracked@yahoo.com>
2023-04-09 12:44:37 -0400

    changed the key name for the value for sending to backend

--- src/front-end/src/components/admin-page/AdminHomePage.js
+++ src/front-end/src/components/admin-page/AdminHomePage.js
@@ -41,7 +41,7 @@ export default class AdminHomePage extends React.Component {
             <Box className="user-home-page-main-box">
                 <Box className="user-home-page-data-box">
                     <Box className="user-home-page-users-name">
-                        <Typography>Hi {this.state.user}, Welcome Back!</Typography>
+                        <Typography>Hi {this.state.user.username}, Welcome Back!</Typography>
                         <Box display="flex" flexGrow={1} />
                         <IconButton color="primary" aria-label="menu" onClick={(e) => this.openMenuDetails(e)} >
                             <MenuIcon fontSize="large" />
