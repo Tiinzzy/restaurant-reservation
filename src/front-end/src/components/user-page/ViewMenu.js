445a2b7dd3d267b1bda648dd78a020cb9f0152c3
Tina <tinzzy.cracked@yahoo.com>
2023-04-11 11:17:21 -0400

    changed some styling for typography

--- src/front-end/src/components/user-page/ViewMenu.js
+++ src/front-end/src/components/user-page/ViewMenu.js
@@ -59,10 +59,10 @@ export default class ViewMenu extends React.Component {
 
                 <Box style={{ display: 'flex', justifyContent: 'space-evenly' }}>
                     <Box className="menu-box-left-side-user">
-                        <Typography fontFamily="serif" fontWeight="bold" fontSize="1.3em" mb={1.6} mt={1.6}>Entrée</Typography>
+                        <Typography fontFamily="serif" fontWeight="bold" fontSize="1.4em" mb={.9}>Entrée</Typography>
                         {this.state.entrees && this.state.entrees.map((e, i) => (
                             <Box key={i} className="each-menu-item-box">
-                                <span style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-betwee' }}>
+                                <span style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
                                     <Typography fontFamily="serif" fontWeight="bold" fontSize="1em" color="rgb(37, 37, 37)">{e.name}</Typography>
                                     <Box display="flex" flexGrow={1} />
                                     <Typography fontFamily="serif" fontWeight="600" fontSize=".9em" color="rgb(37, 37, 37)">${e.price}</Typography>
@@ -70,10 +70,10 @@ export default class ViewMenu extends React.Component {
                                 <Typography fontFamily="serif" fontSize=".8em" color="rgb(94, 94, 94)">{e.description}</Typography>
                             </Box>
                         ))}
-                        <Typography fontFamily="serif" fontWeight="bold" fontSize="1.3em" mb={1.6} mt={1.6}>Main Course</Typography>
+                        <Typography fontFamily="serif" fontWeight="bold" fontSize="1.4em" mb={.9} mt={2.5}>Main Course</Typography>
                         {this.state.mainCourse && this.state.mainCourse.map((e, i) => (
                             <Box key={i} className="each-menu-item-box">
-                                <span style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-betwee' }}>
+                                <span style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
                                     <Typography fontFamily="serif" fontWeight="bold" fontSize="1em" color="rgb(37, 37, 37)">{e.name}</Typography>
                                     <Box display="flex" flexGrow={1} />
                                     <Typography fontFamily="serif" fontWeight="600" fontSize=".9em" color="rgb(37, 37, 37)">${e.price}</Typography>
@@ -83,10 +83,10 @@ export default class ViewMenu extends React.Component {
                         ))}
                     </Box>
                     <Box className="menu-box-right-side-user">
-                        <Typography fontFamily="serif" fontWeight="bold" fontSize="1.3em" mb={1.6} mt={1.6}>Drinks</Typography>
+                        <Typography fontFamily="serif" fontWeight="bold" fontSize="1.4em" mb={.9}>Drinks</Typography>
                         {this.state.drinks && this.state.drinks.map((e, i) => (
                             <Box key={i} className="each-menu-item-box">
-                                <span style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-betwee' }}>
+                                <span style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
                                     <Typography fontFamily="serif" fontWeight="bold" fontSize="1em" color="rgb(37, 37, 37)">{e.name}</Typography>
                                     <Box display="flex" flexGrow={1} />
                                     <Typography fontFamily="serif" fontWeight="600" fontSize=".9em" color="rgb(37, 37, 37)">${e.price}</Typography>
@@ -94,10 +94,10 @@ export default class ViewMenu extends React.Component {
                                 <Typography fontFamily="serif" fontSize=".8em" color="rgb(94, 94, 94)">{e.description}</Typography>
                             </Box>
                         ))}
-                        <Typography fontFamily="serif" fontWeight="bold" fontSize="1.3em" mb={1.6} mt={1.6}>Dessert</Typography>
+                        <Typography fontFamily="serif" fontWeight="bold" fontSize="1.4em" mb={.9} mt={2.5}>Dessert</Typography>
                         {this.state.dessert && this.state.dessert.map((e, i) => (
                             <Box key={i} className="each-menu-item-box">
-                                <span style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-betwee' }}>
+                                <span style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
                                     <Typography fontFamily="serif" fontWeight="bold" fontSize="1em" color="rgb(37, 37, 37)">{e.name}</Typography>
                                     <Box display="flex" flexGrow={1} />
                                     <Typography fontFamily="serif" fontWeight="600" fontSize=".9em" color="rgb(37, 37, 37)">${e.price}</Typography>
@@ -107,9 +107,9 @@ export default class ViewMenu extends React.Component {
                         ))}
                     </Box>
                 </Box>
-                <Box className="menu-box-logo-user">
+                {/* <Box className="menu-box-logo-user">
                     <Typography fontFamily="serif" fontWeight="bold" fontSize="1.5em" onClick={() => this.takeToReservationPage()}>Make a Reservation</Typography>
-                </Box>
+                </Box> */}
             </Box>
         );
     }
