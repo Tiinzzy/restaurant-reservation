af21063db5d098e210d7a7144a817ba5fd60a204
Tina <tinzzy.cracked@yahoo.com>
2023-04-12 17:41:55 -0400

    instead of repeating and setting values of object one by one decided to set the whole object and use object.keys to map it on components

--- src/front-end/src/components/staff-page/EditReservationDialog.js
+++ src/front-end/src/components/staff-page/EditReservationDialog.js
@@ -13,6 +13,9 @@ import BackEndConnection from '../backend-connection/BackEndConnection';
 
 const backend = BackEndConnection.INSTANCE();
 
+
+const INPUIT_FIELDS = ['customer_id', 'cutomer_name'];
+
 export default class EditMenuDialog extends React.Component {
 
     constructor(props) {
@@ -40,64 +43,11 @@ export default class EditMenuDialog extends React.Component {
     componentDidMount() {
         backend.load_reservation(this.state.reservationId, (data) => {
             let that = this;
-            that.setState({
-                customerName: data.cutomer_name, customerId: data.customer_id, date: data.for_date, duration: data.for_how_long, comment: data.latest_comment,
-                reservationType: data.reservation_type, numberOfPeople: data.seat_count, status: data.status, tableId: data.table_id, waiterId: data.waiter_id,
-                tip: data.tip_percent, total: data.total_price, time: data.timestamp
-            });
+
+            Object.keys(data).forEach(k => data[k] = data[k] || '');
+
+            that.setState({ rsvData: data });
         })
-    }
-
-    getCustomerName(e) {
-        this.setState({ customerName: e.target.value, updateError: false });
-    }
-
-    getCustomerId(e) {
-        this.setState({ customerId: e.target.value, updateError: false });
-    }
-
-    getDate(e) {
-        this.setState({ date: e.target.value, updateError: false });
-    }
-
-    getTime(e) {
-        this.setState({ time: e.target.value, updateError: false });
-    }
-
-    getDuration(e) {
-        this.setState({ duration: e.target.value, updateError: false });
-    }
-
-    getComments(e) {
-        this.setState({ comment: e.target.value, updateError: false });
-    }
-
-    getReservationType(e) {
-        this.setState({ reservationType: e.target.value, updateError: false });
-    }
-
-    getSeatCount(e) {
-        this.setState({ numberOfPeople: e.target.value, updateError: false });
-    }
-
-    getTableId(e) {
-        this.setState({ tableId: e.target.value, updateError: false });
-    }
-
-    getStatus(e) {
-        this.setState({ status: e.target.value, updateError: false });
-    }
-
-    getWaiterId(e) {
-        this.setState({ waiterId: e.target.value, updateError: false });
-    }
-
-    getTipPercent(e) {
-        this.setState({ tip: e.target.value, updateError: false });
-    }
-
-    getTotalPrice(e) {
-        this.setState({ total: e.target.value, updateError: false });
     }
 
     cancelAndClose() {
@@ -120,6 +70,12 @@ export default class EditMenuDialog extends React.Component {
         })
     }
 
+    smartChange(e, key) {
+        let data = { ...this.state.rsvData };
+        data[key] = e.target.value;
+        this.setState({ rsvData: data });
+    }
+
     render() {
         return (
             <Box>
@@ -130,45 +86,14 @@ export default class EditMenuDialog extends React.Component {
                     <DialogContentText id="alert-dialog-description">
                         The following are current data, type in textfield to change.
                     </DialogContentText>
-                    <Typography fontWeight='bold' fontSize={14} variant="body1" mb={.5} mt={1.5}>Customer Name: </Typography>
-                    <TextField error={this.state.updateError} size="small" variant="outlined" value={this.state.customerName && this.state.customerName}
-                        onChange={(e) => this.getCustomerName(e)} className="menu-item-detail-text" />
-                    <Typography fontWeight='bold' fontSize={14} variant="body1" mb={.5} mt={1.5}>Customer ID: </Typography>
-                    <TextField error={this.state.updateError} size="small" variant="outlined" value={this.state.customerId && this.state.customerId}
-                        onChange={(e) => this.getCustomerId(e)} className="menu-item-detail-text" />
-                    <Typography fontWeight='bold' fontSize={14} variant="body1" mb={.5} mt={1.5}>Date: </Typography>
-                    <TextField error={this.state.updateError} size="small" variant="outlined" value={this.state.date && this.state.date}
-                        onChange={(e) => this.getDate(e)} className="menu-item-detail-text" />
-                    <Typography fontWeight='bold' fontSize={14} variant="body1" mb={.5} mt={1.5}>Time: </Typography>
-                    <TextField error={this.state.updateError} size="small" variant="outlined" value={this.state.time && this.state.time}
-                        onChange={(e) => this.getTime(e)} className="menu-item-detail-text" />
-                    <Typography fontWeight='bold' fontSize={14} variant="body1" mb={.5} mt={1.5}>Duration: </Typography>
-                    <TextField error={this.state.updateError} size="small" variant="outlined" value={this.state.duration && this.state.duration}
-                        onChange={(e) => this.getDuration(e)} className="menu-item-detail-text" />
-                    <Typography fontWeight='bold' fontSize={14} variant="body1" mb={.5} mt={1.5}>Comments: </Typography>
-                    <TextField error={this.state.updateError} size="small" variant="outlined" value={this.state.comment && this.state.comment}
-                        onChange={(e) => this.getComments(e)} className="menu-item-detail-text" />
-                    <Typography fontWeight='bold' fontSize={14} variant="body1" mb={.5} mt={1.5}>Reservation Type: </Typography>
-                    <TextField error={this.state.updateError} size="small" variant="outlined" value={this.state.reservationType && this.state.reservationType}
-                        onChange={(e) => this.getReservationType(e)} className="menu-item-detail-text" />
-                    <Typography fontWeight='bold' fontSize={14} variant="body1" mb={.5} mt={1.5}>Number of People: </Typography>
-                    <TextField error={this.state.updateError} size="small" variant="outlined" value={this.state.numberOfPeople && this.state.numberOfPeople}
-                        onChange={(e) => this.getSeatCount(e)} className="menu-item-detail-text" />
-                    <Typography fontWeight='bold' fontSize={14} variant="body1" mb={.5} mt={1.5}>Table ID: </Typography>
-                    <TextField error={this.state.updateError} size="small" variant="outlined" value={this.state.tableId && this.state.tableId}
-                        onChange={(e) => this.getTableId(e)} className="menu-item-detail-text" />
-                    <Typography fontWeight='bold' fontSize={14} variant="body1" mb={.5} mt={1.5}>Status: </Typography>
-                    <TextField error={this.state.updateError} size="small" variant="outlined" value={this.state.status && this.state.status}
-                        onChange={(e) => this.getStatus(e)} className="menu-item-detail-text" />
-                    <Typography fontWeight='bold' fontSize={14} variant="body1" mb={.5} mt={1.5}>Waiter ID: </Typography>
-                    <TextField error={this.state.updateError} size="small" variant="outlined" value={this.state.waiterId && this.state.waiterId}
-                        onChange={(e) => this.getWaiterId(e)} className="menu-item-detail-text" />
-                    <Typography fontWeight='bold' fontSize={14} variant="body1" mb={.5} mt={1.5}>Tip Percent: </Typography>
-                    <TextField error={this.state.updateError} size="small" variant="outlined" value={this.state.tip && this.state.tip}
-                        onChange={(e) => this.getTipPercent(e)} className="menu-item-detail-text" />
-                    <Typography fontWeight='bold' fontSize={14} variant="body1" mb={.5} mt={1.5}>Total: </Typography>
-                    <TextField error={this.state.updateError} size="small" variant="outlined" value={this.state.total && this.state.total}
-                        onChange={(e) => this.getTotalPrice(e)} className="menu-item-detail-text" />
+                    {this.state.rsvData && INPUIT_FIELDS.map((k, i) => (
+                        <Box key={i} p={1}>
+                            <Typography fontWeight='bold' fontSize={14} variant="body1" mb={.5} mt={1.5}>{k.replaceAll('_', ' ')}: </Typography>
+                            <TextField error={this.state.updateError} size="small" variant="outlined" value={this.state.rsvData[k]}
+                                onChange={(e) => this.smartChange(e, k)} className="menu-item-detail-text" />
+                        </Box>
+                    ))}
+
                 </DialogContent >
                 <DialogActions>
                     <Button onClick={() => this.cancelAndClose()} variant="contained">Cancel</Button>
