e806a34aef18eb16f9b905a82c214052647533a4
Tina <tinzzy.cracked@yahoo.com>
2023-04-14 11:21:10 -0400

    changed format of displaying data

--- src/front-end/src/components/front-page/Home.js
+++ src/front-end/src/components/front-page/Home.js
@@ -21,14 +21,17 @@ export default class Home extends React.Component {
                 <img src="https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2014/05/1399503143258.jpeg" height={550} />
                 <Box className="home-main-box-about-us">
                     <Typography fontFamily="serif" fontWeight="bold" fontSize="5em" mb={2}>Discover the World of Fine Dining</Typography>
-                    {HOME_PAGE_DATA.map((e, i) => (<Typography key={i} fontFamily="serif" fontSize="1.1em">{e}</Typography>))}
-                    <Box className="home-main-box-buttons">
-                        <a href="/menu">
-                            Checkout Our Menu
-                        </a>
-                        <a href="/reservation">
-                            Make reservation
-                        </a>
+                    <Typography fontFamily="serif" fontWeight="400" fontSize="1.5em" mb={2}>ALWAYS EXCITING, ALWAYS TASTY, ALWAYS FRESH.</Typography>
+                    <Box>
+                        {HOME_PAGE_DATA.map((e, i) => (<Typography key={i} fontFamily="serif" fontSize="1.1em">{e}</Typography>))}
+                        <Box className="home-main-box-buttons">
+                            <a href="/menu">
+                                Checkout Our Menu
+                            </a>
+                            <a href="/reservation">
+                                Make reservation
+                            </a>
+                        </Box>
                     </Box>
                 </Box>
             </Box>
