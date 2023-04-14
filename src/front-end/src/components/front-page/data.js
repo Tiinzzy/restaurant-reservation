57c8ad569fc52dd3720bdd4a6beea5e3d1f96baa
Tina <tinzzy.cracked@yahoo.com>
2023-04-14 13:14:37 -0400

    added extra data to the front page

--- src/front-end/src/components/front-page/data.js
+++ src/front-end/src/components/front-page/data.js
@@ -1,4 +1,6 @@
 export const HOME_PAGE_DATA = [" It is our intention to get back to the roots of hospitality.", 
 "We want each and every guest to feel welcome in our home, and that home is Pane e Vino.", 
 " A destination for classically prepared, meticulously thought out French food and service.", 
-"This no-nonsense approach to fine dining with our combined experience working behind-the-scenes at some of the world’s best restaurants has led us to this point."]
+"This no-nonsense approach to fine dining with our combined experience working behind-the-scenes at some of the world’s best restaurants has led us to this point.",
+"We offers a quick escape to the California coastline, with al fresco dining, a world-renowned infinity pool and unparalleled city and lakeshore views.",
+"Featuring seasonal menus with modern cuisine and refreshing cocktails, guests are welcome seven days a week for breakfast, lunch, midday snacks and dinner."]
